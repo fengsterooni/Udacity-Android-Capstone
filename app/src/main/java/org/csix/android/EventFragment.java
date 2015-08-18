0db4a860a3ba2bef4fe418ffb7a0b0b4e6a4890d
Feng Guo <fengsterooni@gmail.com>
2015-08-18 15:38:11 -0700

    Project: Initial version

---
+++ app/src/main/java/org/csix/android/EventFragment.java
@@ -0,0 +1,22 @@
+package org.csix.android;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class EventFragment extends Fragment {
+
+    public EventFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_event, container, false);
+    }
+}
