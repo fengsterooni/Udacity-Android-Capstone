76e334de59c45e982d28a16b39b375d9c99570e9
Feng Guo <fengsterooni@gmail.com>
2015-08-25 09:24:14 -0700

    Docs: Added comments for using Glide instead of Picasso

--- app/src/main/java/org/csix/android/EventListAdapter.java
+++ app/src/main/java/org/csix/android/EventListAdapter.java
@@ -69,7 +69,22 @@ public class EventListAdapter extends CursorAdapter {
 
         String imageUrl = cursor.getString(cursor.getColumnIndex(CSixContract.EventEntry.COLUMN_IMAGE));
         if (imageUrl != null) {
+            // The reason I did not use Picasso
+            /*
+            java.lang.NoSuchMethodError: No virtual method setConnectTimeout(JLjava/util/concurrent/TimeUnit;)V in class Lcom/squareup/okhttp/OkHttpClient; or its super classes (declaration of 'com.squareup.okhttp.OkHttpClient' appears in /data/app/org.csix.android-1/base.apk)
+            at com.squareup.picasso.OkHttpDownloader.defaultOkHttpClient(OkHttpDownloader.java:32)
+            at com.squareup.picasso.OkHttpDownloader.<init>(OkHttpDownloader.java:76)
+            at com.squareup.picasso.OkHttpDownloader.<init>(OkHttpDownloader.java:55)
+            at com.squareup.picasso.OkHttpDownloader.<init>(OkHttpDownloader.java:45)
+            at com.squareup.picasso.Utils$OkHttpLoaderCreator.create(Utils.java:424)
+            at com.squareup.picasso.Utils.createDefaultDownloader(Utils.java:250)
+            at com.squareup.picasso.Picasso$Builder.build(Picasso.java:832)
+            at com.squareup.picasso.Picasso.with(Picasso.java:662)
+            at org.csix.android.EventListAdapter.bindView(EventListAdapter.java:72)
+            */
             // Picasso.with(context).load(imageUrl).into(viewHolder.speakerImage);
+
+            // Use Glide instead of image loading
             Glide.with(context).load(imageUrl).into(viewHolder.speakerImage);
         }
     }
